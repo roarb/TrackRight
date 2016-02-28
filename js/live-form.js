347a80768153c3a8dbeed49d661e4bd2bef2d2d3
roarb <rhoehn24@gmail.com>
2016-02-28 13:07:03 -0600

    sample form updates with working js

--- js/live-form.js
+++ js/live-form.js
@@ -2,9 +2,31 @@
  * Created by rhoeh on 2/28/2016.
  */
 
-function updateForm(e){
-    console.log(e);
-    console.log(e.target);
-    console.log(e.target.val());
-    console.log(student_daily_entry);
+
+
+function updateForm(e) {
+    var item = e.target;
+    var matrixId = $(item).attr('id');
+    var matrix = matrixId.split('-'),
+        value = $(item).val();
+
+    applyUpdates(matrix, value);
+}
+
+function applyUpdates(matrix, value) {
+    student_daily_entry.classes[matrix[0]].goals[matrix[1]].value = value;
+    processTotals();
+}
+
+function processTotals() {
+    var grandTotal = 0;
+    $('.total-points').each(function(key, val){
+        var newTotal = 0;
+        $(student_daily_entry.classes[key].goals).each(function(key, rating){
+            newTotal += parseInt(rating.value);
+            grandTotal += parseInt(rating.value);
+        });
+        $(val).text(newTotal);
+    });
+    $('#grand-total').text(grandTotal);
 }
